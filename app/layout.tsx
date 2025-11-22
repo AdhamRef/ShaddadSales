e6dd68df10b7baa9552e15cd0a481b59bae774f0
AdhamRef <developedbyadham7@gmail.com>
2025-11-22 07:36:19 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,25 +14,8 @@ export const metadata: Metadata = {
   title: 'Shaddad Sales Panel',
   description: 'تتبع الأنشطة اليومية وإدارة العملاء المؤهلين ومراقبة درجات KPI لفريق المبيعات الخاص بك',
   generator: 'v0.app',
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
 }
-
+//asd
 export default function RootLayout({
   children,
 }: Readonly<{
